73f55d34a869d058980abf8f180d1e03e4c357a8
Nightlamp1 <robert.britt2011@gmail.com>
2016-09-15 23:40:57 -0500

    End of chapter 1

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+    
+'''
 def post_list(request):
     object_list = Post.published.all()
     paginator = Paginator(object_list,3)#Indicates 3 post per Page
@@ -17,7 +26,7 @@ def post_list(request):
         posts=paginator.page(paginator.num_pages)
 
     return render(request,'blog/post/list.html',{'page':page,'posts':posts})
-
+'''
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
                              slug=post,
