00c4f822c843aefe1bb42275e09316f32cbd18e7
Nightlamp1 <robert.britt2011@gmail.com>
2016-09-16 00:36:33 -0500

    Created basic email form

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from .forms import EmailPostForm
 from django.views.generic import ListView
 
 # Create your views here.
@@ -10,23 +11,7 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
-    
-'''
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list,3)#Indicates 3 post per Page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        #If page is not an int return first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        #If page is out of range return last page
-        posts=paginator.page(paginator.num_pages)
 
-    return render(request,'blog/post/list.html',{'page':page,'posts':posts})
-'''
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
                              slug=post,
@@ -35,3 +20,17 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request,'blog/post/detail.html',{'post':post})
+
+def post_share(request,post_id):
+    #Retrieve post by id
+    post = get_object_or_404(Post,id=post_id, status='published')
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            #Form fields are all valid
+            cd = form.cleaned_data
+            #... send email here
+    else:
+        form = EmailPostForm()
+
+    return render(request,'blog/post/share.html',{'post':post,'form':form})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -101,3 +101,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Email SMTP server information
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'robert.britt2011@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
